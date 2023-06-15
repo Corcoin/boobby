84ff204b46ae52a486deeae86a1636fec816d575
brian wesson <89755034+Corcoin@users.noreply.github.com>
2023-06-15 03:47:06 -0500

    push

---
+++ Admin.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="WebApplication7.Admin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="formAdminPage" runat="server">
+        <div>
+            <h2>Admin Page</h2>
+            <table id="tblChanges" runat="server">
+                <thead>
+                    <tr>
+                        <th>Cost Center</th>
+                        <th>Change Type</th>
+                        <th>Timestamp</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Populate table rows dynamically using server-side code -->
+                </tbody>
+            </table>
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ Admin.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace WebApplication7
+{
+    public partial class Admin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Populate the changes table with the recorded changes
+                PopulateChangesTable();
+            }
+        }
+
+        private void PopulateChangesTable()
+        {
+            // Connect to the SQL Server database
+            using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\wired\\source\\repos\\WebApplication7\\WebApplication7\\App_Data\\Database1.mdf;Integrated Security=True"))
+            {
+                // Construct the SQL query
+                string query = "SELECT costcenter, changetype, timestamp FROM changes";
+
+                // Create a SqlCommand object and set the query and connection
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Open the connection
+                    connection.Open();
+
+                    // Execute the query and get the results
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Iterate over the result set and populate the table rows dynamically
+                        while (reader.Read())
+                        {
+                            string costCenter = reader["costcenter"].ToString();
+                            string changeType = reader["changetype"].ToString();
+                            string timestamp = reader["timestamp"].ToString();
+
+                            // Create a new table row
+                            HtmlTableRow row = new HtmlTableRow();
+
+                            // Create table cells for each column
+                            HtmlTableCell cellCostCenter = new HtmlTableCell();
+                            cellCostCenter.InnerText = costCenter;
+                            row.Cells.Add(cellCostCenter);
+
+                            HtmlTableCell cellChangeType = new HtmlTableCell();
+                            cellChangeType.InnerText = changeType;
+                            row.Cells.Add(cellChangeType);
+
+                            HtmlTableCell cellTimestamp = new HtmlTableCell();
+                            cellTimestamp.InnerText = timestamp;
+                            row.Cells.Add(cellTimestamp);
+
+                            // Add the row to the changes table
+                            tblChanges.Rows.Add(row);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
